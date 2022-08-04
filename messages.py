32d2ba0582012cee60868e14f32cbb6e3d9cf808
James-L-Liu <jliu613@aucklanduni.ac.nz>
2022-08-05 04:01:35 +1200

    Now another sentence is updated

--- messages.py
+++ messages.py
@@ -4,7 +4,7 @@ LOSE = "Haha, you lose!"
 PICK_NUMBER = "Now, the sentence has been modified..."
 PROMPT_GUESS = "Now your turn to guess!\nEnter an even number between 1 and 100: "
 PLAY_AGAIN = "Would you like to play again? (Type 'y' for yes or  'n' for no.): "
-INVALID_INPUT = "That is not a valid answer..."
+INVALID_INPUT = "That is not a valid answer... or maybe it is"
 LINE_OF_STARS = "**************************************"
 
 def final_score(gamesWon, gamesPlayed):
